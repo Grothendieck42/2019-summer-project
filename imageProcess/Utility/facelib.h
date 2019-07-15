44b36528d325415aed32a6a4eab8d8a0720266e2
codyly <china_cody@hotmail.com>
2019-07-15 14:11:46 +0800

    OPENCV3

--- imageProcess/Utility/facelib.h
+++ imageProcess/Utility/facelib.h
@@ -5,7 +5,6 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/objdetect.hpp>
-#include <opencv/cv.h>
 #include <fstream>
 #include <iostream>
 #include <QProgressDialog>
