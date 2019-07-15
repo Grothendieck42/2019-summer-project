41fa353b1f844de7d7073f29366474e58b934e56
VamosC <3160102243@zju.edu.cn>
2019-07-15 13:56:24 +0800

    add

--- imageProcess/View/dialog.cpp
+++ imageProcess/View/dialog.cpp
@@ -1,15 +1,43 @@
 #include "dialog.h"
 #include "ui_dialog.h"
 
-Dialog::Dialog(const QString &name, QWidget *parent) :
+Dialog::Dialog(const QString &title, const QString &name, int min ,int max, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
-    this->setWindowTitle("调整" + name);
+    this->setWindowTitle(title);
     ui->setupUi(this);
+    label = this->findChild<QLabel*>("label");
+    slider = this->findChild<QSlider*>("horizontalSlider");
+    board = this->findChild<QSpinBox*>("spinBox");
+    label->setText(name);
+    slider->setMinimum(min);
+    slider->setMaximum(max);
+    board->setMinimum(min);
+    board->setMaximum(max);
 }
 
 Dialog::~Dialog()
 {
     delete ui;
+}
+
+void Dialog::on_buttonBox_accepted()
+{
+
+}
+
+void Dialog::on_buttonBox_rejected()
+{
+
+}
+
+void Dialog::on_horizontalSlider_valueChanged(int value)
+{
+    board->setValue(value);
+}
+
+void Dialog::on_spinBox_valueChanged(int arg1)
+{
+    slider->setValue(arg1);
 }
--- imageProcess/View/dialog.h
+++ imageProcess/View/dialog.h
@@ -2,7 +2,10 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QLabel>
 #include <QString>
+#include <QSlider>
+#include <QSpinBox>
 
 namespace Ui {
 class Dialog;
@@ -13,11 +16,23 @@ class Dialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit Dialog(const QString &name, QWidget *parent = nullptr);
+    explicit Dialog(const QString &title, const QString &name, int min = 1, int max = 100, QWidget *parent = nullptr);
     ~Dialog();
+
+private slots:
+    void on_buttonBox_accepted();
+
+    void on_buttonBox_rejected();
+
+    void on_horizontalSlider_valueChanged(int value);
+
+    void on_spinBox_valueChanged(int arg1);
 
 private:
     Ui::Dialog *ui;
+    QLabel *label;
+    QSlider *slider;
+    QSpinBox *board;
 };
 
 #endif // DIALOG_H
